9933c82d84d1f49db8b14c2a0a44551bcf903adf
Tirthankar Nath <tirthankar.nath2@gmail.com>
2024-01-19 23:09:32 +0530

    past sponsers

--- src/Sections/Team/index.js
+++ src/Sections/Team/index.js
@@ -2,13 +2,29 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import img1 from "../../assets/img1.png";
-import img2 from "../../assets/img2.jpeg";
-import img3 from "../../assets/img3.jpeg";
-import img6 from "../../assets/img6.jpg";
-import img4 from "../../assets/img4.png";
-import img5 from "../../assets/img5.jpeg";
+// import img1 from "../../assets/img1.png";
+// import img2 from "../../assets/img2.jpeg";
+// import img3 from "../../assets/img3.jpeg";
+// import img6 from "../../assets/img6.jpg";
+// import img4 from "../../assets/img4.png";
+// import img5 from "../../assets/img5.jpeg";
 import Party from '../../components/Party';
+
+import DigitalOceanImg from "../../assets/past_sponsers/DigitalOcean.png";
+import GeeksforGeeksImg from "../../assets/past_sponsers/GeeksforGeeks.png";
+import JBsImg from "../../assets/past_sponsers/JBs.png";
+import LottiGhottiImg from "../../assets/past_sponsers/LottiGhotti.png";
+import NewtonSchoolImg from "../../assets/past_sponsers/NewtonSchool.png";
+import OREILLYImg from "../../assets/past_sponsers/OREILLY.png";
+import OlatusImg from "../../assets/past_sponsers/Olatus.png";
+import PoyntSportsImg from "../../assets/past_sponsers/Poynt_Sports.png";
+import techvariableImg from "../../assets/past_sponsers/techvariable.png";
+import UjucodeImg from "../../assets/past_sponsers/Ujucode.png";
+import XipaarImg from "../../assets/past_sponsers/Xipaar.png";
+import XopuntechImg from "../../assets/past_sponsers/Xopuntech.png";
+
+
+
 
 const Section = styled.div`
   min-height: 100vh;
@@ -105,14 +121,20 @@ function Team() {
   return (
     <Section id='team'>
         <Party/>
-      <Title>Team</Title>
+      <Title>PAST SPONSORS</Title>
       <Container>
-        <MemberComponent img={img1} name="Abhinash" position="CC Lead" />
-        <MemberComponent img={img2} name="Parthiv" position="GDSC Lead" />
-        <MemberComponent img={img3} name="Radhika" position="..." />
-        <MemberComponent img={img4} name="Bidyasagar" position="..." />
-        <MemberComponent img={img5} name="Tirthankar" position="..." />
-        <MemberComponent img={img6} name="Jyotirmoy" position="Smile in Pain" />
+        <MemberComponent img={DigitalOceanImg} name="Digital Ocean" position="" />
+        <MemberComponent img={GeeksforGeeksImg} name="GeeksforGeeks" position="" />
+        <MemberComponent img={JBsImg} name="JB's" position="" />
+        <MemberComponent img={LottiGhottiImg} name="Lotti Ghotti" position="" />
+        <MemberComponent img={NewtonSchoolImg} name="Newton School" position="" />
+        <MemberComponent img={OREILLYImg} name="O'REILLY" position="" />
+        <MemberComponent img={OlatusImg} name="Olatus" position="" />
+        <MemberComponent img={PoyntSportsImg} name="Poynt Sports" position="" />
+        <MemberComponent img={techvariableImg} name="Tech Variable" position="" />
+        <MemberComponent img={UjucodeImg} name="Ujucode" position="" />
+        <MemberComponent img={XipaarImg} name="XiPaar" position="" />
+        <MemberComponent img={XopuntechImg} name="Xopuntech" position="" />
       </Container>
     </Section>
   );
